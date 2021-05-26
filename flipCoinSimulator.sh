74b62b9c52c638d2afadd951a4cc08f54000b03f
kunallad54 <kunallad5473@gmail.com>
2021-05-26 17:16:13 +0530

    Flipped Coin 21 times extended with tie condition

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -10,9 +10,13 @@ do
 		headCount=$((headCount + 1))
 		if [ $headCount -eq 21 ]
 		then
+			diff=$((headCount - tailCount))
+			if [ $headCount -eq $tailCount ]
+			then
+				continue
+			fi
 			echo "Head Count is : $headCount"
 			echo "Tail Count is : $tailCount"
-			diff=$((headCount - tailCount))
 			echo "Head has tossed 21 times and won by $diff"
 			break
 		fi
@@ -20,6 +24,10 @@ do
 		tailCount=$((tailCount + 1))
 		if [ $tailCount -eq 21 ]
 		then
+			if [ $headCount -eq $tailCount ]
+			then
+				continue
+			fi
 			echo "Head Count is : $headCount"
 			echo "Tail Count is : $tailCount"
 			diff=$((tailCount - headCount))
