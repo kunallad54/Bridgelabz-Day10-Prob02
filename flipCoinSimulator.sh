76d55f464e133a9d65101d4a03e0ca979e1b273c
kunallad54 <kunallad5473@gmail.com>
2021-05-26 16:50:11 +0530

    Flipped coin till either of them(heads or tails) reached 21 times

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,15 +2,30 @@
 
 headCount=0
 tailCount=0
-for(( i=1;i<=50;i++ ))
+while [ 1 ]
 do
 	coinResult=$((RANDOM%2))
 	if [ $coinResult -eq 1 ]
 	then
 		headCount=$((headCount + 1))
+		if [ $headCount -eq 21 ]
+		then
+			echo "Head Count is : $headCount"
+			echo "Tail Count is : $tailCount"
+			diff=$((headCount - tailCount))
+			echo "Head has tossed 21 times and won by $diff"
+			break
+		fi
 	else
 		tailCount=$((tailCount + 1))
+		if [ $tailCount -eq 21 ]
+		then
+			echo "Head Count is : $headCount"
+			echo "Tail Count is : $tailCount"
+			diff=$((tailCount - headCount))
+			echo "Tail has tossed 21 times and won by $diff"
+			break
+		fi
+
 	fi
 done
-echo "Total time head won : $headCount"
-echo "Total time tail won : $tailCount"
